663a14a223781e8f7862459b07e2bd671d45b8ca
halos <halos@lambda>
2010-12-25 12:07:09 +0100

    Arreglando warnings y errors

--- main.c
+++ main.c
@@ -1,10 +1,39 @@
 #include <stdio.h>
+#include <string.h>
+
+#include "eedd/vdin_str.h"
+#include "ai_buscador.h"
 
 int main(int argc,char** argv)
 {
 
+    char *stoplist;
 
-	return 0;
+    char query[] = "dell laptop functions";
+    vdin_str consulta, vs_free;
+
+    //stoplist = argv[2];
+    stoplist = strdup("englishST.txt");
+
+    consulta = split_text(query);
+
+    // TODO: Liberar las cadenas
+    // stemmer
+    vs_free = consulta;
+    consulta = ai_buscador_stemmer(consulta);
+    vdin_str_destruye(&vs_free);
+
+    // stopper
+    vs_free = consulta;
+    consulta = ai_buscador_stopper(consulta);
+    vdin_str_destruye(&vs_free);
+
+    // normaliza
+    vs_free = consulta;
+    consulta = ai_buscador_normaliza(consulta);
+    vdin_str_destruye(&vs_free);
+
+    return 0;
 
 }
 
