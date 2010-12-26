d0a84495d11b27de9e2666fef7e65385844edfc6
halos <halos@lambda>
2010-12-26 12:17:21 +0100

    En proceso de refactorización

---
+++ eedd/listadin_freqs.h
@@ -0,0 +1,40 @@
+/**
+ * @file listadin_freqs.h
+ * @brief Fichero cabecera del TDA lista dinamica de freq_file
+ * @date 22/10/2010
+ * @version 1.0
+*/
+
+#ifndef __LISTADIN_freqs_H
+#define __LISTADIN_freqs_H
+
+#include "../estructuras2.h"
+
+
+/** Comentar para eliminar las comprobaciones de precondiciones en las operaciones del tipo */
+#define _LISTADIN_idf_DEBUG
+
+/**
+ * @brief TDA lista dinamica
+ * @par Descripcion
+ *   Definir variables del tipo tlistadin_freqs para trabajar con listas dinamicas
+*/
+
+typedef struct tlistadin_freqs_rep *listadin_idf;
+
+	listadin_idf listadin_idf_crea();
+	void listadin_idf_destruye(listadin_idf *l);
+	int listadin_idf_numElem(listadin_idf l);
+	void listadin_idf_insertaPos(listadin_idf l, freq_file *elem, int pos);
+	void listadin_idf_insertaInicio(listadin_idf l, freq_file *elem);
+	void listadin_idf_insertaFinal(listadin_idf l, freq_file *elem);
+	void listadin_idf_inserta(listadin_idf l, freq_file *elem);
+	void listadin_idf_sacaPos(listadin_idf l, freq_file **elem, int pos);
+	void listadin_idf_obtienePos(listadin_idf l, freq_file **elem, int pos);
+	int listadin_idf_busca(listadin_idf l, freq_file **elem, int (*fcomp)(const freq_file **elem1, const freq_file **elem2));
+
+	void listadin_idf_iter_ini (listadin_idf l);
+	freq_file** listadin_idf_iter_siguiente (listadin_idf l);
+	int listadin_idf_iter_fin(listadin_idf l);
+
+#endif
---
+++ estructuras2.h
@@ -0,0 +1,19 @@
+/* 
+ * File:   estructuras2.h
+ * Author: halos
+ *
+ * Created on 14 de noviembre de 2010, 19:48
+ */
+
+#ifndef ESTRUCTURAS2_H
+#define	ESTRUCTURAS2_H
+
+struct idf_file_struct{
+    int file;
+    float idf;
+};
+
+typedef struct idf_file_struct idf_file;
+
+#endif	/* ESTRUCTURAS2_H */
+
